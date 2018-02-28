5d0d992a2361b4481c412bb35acfd9ab5e2f864a
xavier_zy <mid_earth123@outlook.com>
2018-02-28 16:58:29 +0800

    在overview页面完成搜索周边并表格展示功能

--- searchPOI.js
+++ searchPOI.js
@@ -5,22 +5,22 @@ function searchPOIandDrawOnMap(map, markers, layerControl) {
 
     var radius = $('#radius').val();
     var keywords = [];
-    if($('#inlineCheckbox1').is(':checked')) {
+    if ($('#inlineCheckbox1').is(':checked')) {
         keywords.push("酒店");
-    }; 
-    if($('#inlineCheckbox2').is(':checked')) {
+    };
+    if ($('#inlineCheckbox2').is(':checked')) {
         keywords.push("公园");
-    }; 
-    if($('#inlineCheckbox3').is(':checked')) {
+    };
+    if ($('#inlineCheckbox3').is(':checked')) {
         keywords.push("便利店");
     };
-    if($('#inlineCheckbox4').is(':checked')) {
+    if ($('#inlineCheckbox4').is(':checked')) {
         keywords.push("地铁站");
     };
 
     var options = {
-    pageCapacity: 100,
-        onSearchComplete: function(results) {
+        pageCapacity: 100,
+        onSearchComplete: function (results) {
             // 判断状态是否正确
             if (local.getStatus() == BMAP_STATUS_SUCCESS) {
                 if (results.length == 0) {
@@ -30,7 +30,9 @@ function searchPOIandDrawOnMap(map, markers, layerControl) {
 
                 markers.clearLayers();
                 for (var index in results) {
+                    var resultsList = [];
                     for (var i = 0; i < results[index].getCurrentNumPois(); i++) {
+                        resultsList.push(results[index].getPoi(i));
                         var dic = new Array();
                         var wgsCoor = bd09towgs84(results[index].getPoi(i).point.lng, results[index].getPoi(i).point.lat);
                         dic['lon'] = wgsCoor[0];
@@ -45,36 +47,39 @@ function searchPOIandDrawOnMap(map, markers, layerControl) {
                                 prefix: 'fa',
                                 markerColor: 'green'
                             });
-                        } else if (results[index].keyword == "酒店"){
+                        } else if (results[index].keyword == "酒店") {
                             markerIcon = L.AwesomeMarkers.icon({
                                 icon: 'h-square',
                                 prefix: 'fa',
                                 markerColor: 'red'
-                            }); 
-                        } else if (results[index].keyword == "便利店"){
+                            });
+                        } else if (results[index].keyword == "便利店") {
                             markerIcon = L.AwesomeMarkers.icon({
                                 icon: 'shopping-basket',
                                 prefix: 'fa',
                                 markerColor: 'blue'
-                            }); 
-                        } else if (results[index].keyword == "地铁站"){
+                            });
+                        } else if (results[index].keyword == "地铁站") {
                             markerIcon = L.AwesomeMarkers.icon({
                                 icon: 'subway',
                                 prefix: 'fa',
                                 markerColor: 'darkpurple'
-                            }); 
-                        }  else {
+                            });
+                        } else {
                             markerIcon = L.AwesomeMarkers.icon({
                                 prefix: 'fa',
                                 markerColor: 'cadetblue'
-                            }); 
+                            });
                         }
 
-                        var marker = L.marker([dic['lat'], dic['lon']], {icon: markerIcon});
+                        var marker = L.marker([dic['lat'], dic['lon']], {
+                            icon: markerIcon
+                        });
                         marker.addTo(markers);
                         var popContent = L.Util.template('<p>{title}<br />{address}<br />{lat}, {lon}</p>', dic);
                         marker.bindPopup(popContent);
                     }
+                    showResultsInTable(resultsList);
                 }
             }
 
@@ -88,4 +93,22 @@ function searchPOIandDrawOnMap(map, markers, layerControl) {
     };
     var local = new BMap.LocalSearch("上海", options);
     local.searchNearby(keywords, new BMap.Point(bd_coor[0], bd_coor[1]), Number(radius));
+}
+
+function showResultsInTable(sResults) {
+    for (var i = 0; i < sResults.length; i++) {
+        var rTitle = $("<td></td>").text(sResults[i].title);
+        var rAddress = $("<td></td>").text(sResults[i].address);
+        var rPhoneNumber = $("<td></td>").text(sResults[i].phoneNumber);
+        var rTags = $("<td></td>").text(sResults[i].tags.toString());
+
+        rTitle.wrapInner("<a href='" + sResults[i].detailUrl + "'></a>");
+        var tr = document.createElement("TR");
+        rTitle.appendTo(tr);
+        rAddress.appendTo(tr);
+        rPhoneNumber.appendTo(tr);
+        rTags.appendTo(tr);
+
+        $("#results-list").append(tr);
+    }
 }
